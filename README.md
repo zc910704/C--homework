93fb06f60a8dc9822e9189c0fd4035ba9210cb5b
zc910704 <329188614@qq.com>
2019-03-12 22:15:21 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# C--homework
-collection of C#  learning homework 
+# C sharp homework
+collection of C Sharp  learning homework 
+
+- 爬虫
+
+```
+一个简单的异步爬虫。
+1. 主线程首先异步调用GetMatches函数获得首页的内容，并用正则表达式提取要下载网址，return一个网址的hashtable
+2. GetMatches函数完成后在getMatchCompleteCallback的回调函数中,获得hashtable，并异步调用downloadMatchAction下载文件，通过回调函数结束调用
+3. 除了主线程，其他线程需要通过调用主线程更新界面
+4. 4.网址Url通过类的属性来检查输入
+
+```
+
---
+++ resource/爬虫/爬虫/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace 爬虫
+{
+    public partial class Form1 : Form
+    {
+        Hashtable hashtable = new Hashtable();
+        private string _url;
+        public string Url
+        {
+            get
+            {
+                return _url;
+            }
+            set
+            {
+                if (value.StartsWith(@"http://")|value.StartsWith(@"https://"))
+                {
+                    _url = value;
+                }
+                else
+                {
+                    _url = @"http://" + value;
+                }
+            }
+        }
+     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public delegate Hashtable GetMatchDelegate(string s);
+        public delegate void DownloadMatchDelegate(string s,string fileName);
+        //点击按钮时入口
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetMatchDelegate GetUrlMatchesDelegate = GetMatches;
+            treeView1.Nodes.Add("正在下载首页...");
+            GetUrlMatchesDelegate.BeginInvoke(Url, getMatchCompleteCallback, GetUrlMatchesDelegate);
+        }
+        //被委托的获得hasttable的函数
+        private Hashtable GetMatches(string s)
+        {
+            Url = textBox1.Text;
+            WebClient client = new WebClient();
+            byte[] pageData = client.DownloadData(Url);
+            string Htmlpage = Encoding.Default.GetString(pageData);
+            client.Dispose();
+            Regex rx = new Regex(@"(?:(?:http|ftp|https)://)(?:(?:[a-zA-Z0-9\._-]+\.[a-zA-Z]{2,6})|(?:[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,4})*(?<fileName>/[a-zA-Z0-9\&%_\./-~-]*)?");
+            MatchCollection ms = rx.Matches(Htmlpage);
+            foreach (Match m in ms)
+            {
+                try
+                {
+                    hashtable.Add(m.Value, false);
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+            return hashtable;
+        }
+
+        private void DownloadMatchcompleteCallback(IAsyncResult ar)
+        {
+            (ar.AsyncState as DownloadMatchDelegate).EndInvoke(ar);
+           
+        }
+
+        private void downloadMatchAction(string s,string fileName)
+        {
+            if (imagecontrol.Checked)
+            {
+                if (fileName.EndsWith("jpg"))
+                {
+                    string msg = "下载完成：" + s;
+                    WebClient client = new WebClient();
+                    try
+                    {
+                        if (fileName != "")
+                        {
+                            client.DownloadFile(s, fileName);
+
+                            client.Dispose();
+                        }
+
+                    }
+                    catch (Exception e)
+                    {
+
+                    }
+                    if (InvokeRequired)
+                    {
+                        this.BeginInvoke(new AddMsgsDelegate(AddMsgs), new object[] { msg });
+                    }
+                    else
+                    {
+                        this.AddMsgs(msg);
+                    }
+                }
+            }
+            else
+            {
+                string msg = "下载完成：" + s;
+                WebClient client = new WebClient();
+                try
+                {
+                    if (fileName != "")
+                    {
+                        client.DownloadFile(s, fileName);
+
+                        client.Dispose();
+                    }
+
+                }
+                catch (Exception e)
+                {
+
+                }
+
+
+                if (InvokeRequired)
+                {
+                    this.BeginInvoke(new AddMsgsDelegate(AddMsgs), new object[] { msg });
+                }
+                else
+                {
+                    this.AddMsgs(msg);
+                }
+            }
+            
+        }
+
+        private string getFileName(string s)
+        {
+            if (s != null)
+            {
+                string fileName;
+                Match m = Regex.Match(s, @"[0-9a-zA-Z&=-]{1,}?\.(png|php|html|xml|js|jpg)");
+                fileName = m.Value;
+                return fileName;
+            }
+            else return null;
+
+        }
+
+        private void getMatchCompleteCallback(IAsyncResult iar)
+        {
+            Hashtable hashtable = (iar.AsyncState as GetMatchDelegate).EndInvoke(iar);
+            string msg = hashtable.Count.ToString() + "个所有类型链接";
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new AddMsgsDelegate(AddMsgs),new object[] { msg });
+            }
+            else
+            {
+                this.AddMsgs(msg);
+            }
+            string fileName;
+            foreach(DictionaryEntry de in hashtable)
+            {
+                 fileName = this.getFileName(de.Key as string);
+                if (fileName == null) break;
+                 DownloadMatchDelegate downloadMatchdelegate = downloadMatchAction;
+                 downloadMatchdelegate.BeginInvoke(de.Key as string, fileName, DownloadMatchcompleteCallback, downloadMatchdelegate);
+            
+            }
+        }
+        private delegate void AddMsgsDelegate(String msg);
+        private void AddMsgs(string msg)
+        {
+            treeView1.Nodes.Add(msg);
+        }
+    }
+}
